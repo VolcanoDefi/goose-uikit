bf7db5cc55260aca3f50cf49136b1701d931ccac
Aydwee <81417990+Aydwee@users.noreply.github.com>
2021-04-06 11:14:52 +0200

    update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2797,9 +2797,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#85OC1E",
+    primaryBright: "#85OC1E",
+    primaryDark: "#85OC1E",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
