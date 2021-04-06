6e65dca9aa47fe29267aaa211235b8876240e6a3
Aydwee <81417990+Aydwee@users.noreply.github.com>
2021-04-06 11:26:59 +0200

    colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -36,7 +36,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-    secondary: "#850C1E",
+    secondary: "#C5B358",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
@@ -44,7 +44,7 @@ export const darkColors: Colors = {
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-    text: "#850C1E",
+    text: "#C5B358",
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#524B63",
